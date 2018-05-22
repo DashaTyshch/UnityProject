43b4a50c9f6f4494439bcf46180f79cac818e5b1
DashaTyshch <dasha.tyshenko98@gmail.com>
2018-05-22 17:40:07 +0300

    DZ1

---
+++ Assets/Scripts/HeroRabbit.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroRabbit : MonoBehaviour {
+	public float speed = 3;
+	float value = 0;
+	Rigidbody2D myBody = null;
+
+	void Awake() {
+		value = Input.GetAxis("Horizontal");
+	}
+
+	// Use this for initialization
+	void Start () {
+		myBody = this.GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void FixedUpdate () {
+		value = Input.GetAxis("Horizontal");
+		if (Mathf.Abs (value) > 0) {
+			Vector2 vel = myBody.velocity;
+			vel.x = value * speed;
+			myBody.velocity = vel;
+		}
+
+		SpriteRenderer sr = GetComponent<SpriteRenderer> ();
+		if (value < 0) {
+			sr.flipX = true;
+		} else if(value > 0) {
+			sr.flipX = false;
+		}
+	}
+}
